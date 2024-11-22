0b1c329bf3c751a23ca012b6d10222d8e27bd648
SathvaraDhruvin07 <sathvaradhruvin05@gmail.com>
2024-11-22 13:20:19 +0530

    first-commit

---
+++ assests/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggler = document.querySelector(".navbar-toggler");
+    const offcanvas = document.getElementById("offcanvasExample");
+
+    toggler.addEventListener("click", () => {
+        const screenWidth = window.innerWidth;
+        if (screenWidth < 992) {
+            const offcanvasInstance = new bootstrap.Offcanvas(offcanvas);
+            offcanvasInstance.show();
+        }
+    });
+});
